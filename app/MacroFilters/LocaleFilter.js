ea127077cfec2dcd119712b96e80e81b02c1fb6c
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-07-15 13:49:43 +0800

    Update LocaleFilter.js 要記得做編譯

--- app/MacroFilters/LocaleFilter.js
+++ app/MacroFilters/LocaleFilter.js
@@ -1,4 +1,4 @@
 module.exports = function (macro) {
   return macro.split(`<if locale="[ZH_LOCALE]" match="any">`)
-    .join(`<if locale="zh jp chi Chinese 中文 繁體中文 簡體中文" match="any">`)
+    .join(`<if locale="zh chi Chinese 中文 繁體中文 簡體中文 up Jon 日文 日本 ko kor 韓國 韓文" match="any">`)
 }
