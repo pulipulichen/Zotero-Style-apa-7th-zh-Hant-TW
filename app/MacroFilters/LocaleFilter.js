ac8739fb71ee3f140e2ef5b10f9919e51965a274
Pulipuli Chen <pulipuli.chen@gmail.com>
2023-07-16 14:56:09 +0800

    locale

--- app/MacroFilters/LocaleFilter.js
+++ app/MacroFilters/LocaleFilter.js
@@ -1,4 +1,4 @@
 module.exports = function (macro) {
   return macro.split(`<if locale="[ZH_LOCALE]" match="any">`)
-    .join(`<if locale="zh chi Chinese 中文 繁體中文 簡體中文 up Jon 日文 日本 ko kor 韓國 韓文" match="any">`)
+    .join(`<if locale="zh chi Chinese 中文 正體中文 繁體中文 簡體中文 简体中文 jp jpn 日文 日本 ko kor 韓國 韓文" match="any">`)
 }
